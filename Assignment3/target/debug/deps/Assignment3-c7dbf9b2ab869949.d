9c27f119946df9eb34b0088c3f9e849ca79deb94
Dheerajknoldus <dheeraj.mishra@knoldus.com>
2021-02-25 23:44:48 +0530

    added sorting

--- Assignment3/target/debug/deps/Assignment3-c7dbf9b2ab869949.d
+++ Assignment3/target/debug/deps/Assignment3-c7dbf9b2ab869949.d
@@ -1,7 +1,8 @@
-/home/knoldus/Desktop/KUP_assignment/Assignment3/target/debug/deps/Assignment3-c7dbf9b2ab869949: src/main.rs src/searching.rs src/leap_year.rs
+/home/knoldus/Desktop/KUP_assignment/KUPAssignment/Assignment3/target/debug/deps/Assignment3-c7dbf9b2ab869949: src/main.rs src/searching.rs src/leap_year.rs src/sorting.rs
 
-/home/knoldus/Desktop/KUP_assignment/Assignment3/target/debug/deps/Assignment3-c7dbf9b2ab869949.d: src/main.rs src/searching.rs src/leap_year.rs
+/home/knoldus/Desktop/KUP_assignment/KUPAssignment/Assignment3/target/debug/deps/Assignment3-c7dbf9b2ab869949.d: src/main.rs src/searching.rs src/leap_year.rs src/sorting.rs
 
 src/main.rs:
 src/searching.rs:
 src/leap_year.rs:
+src/sorting.rs:
