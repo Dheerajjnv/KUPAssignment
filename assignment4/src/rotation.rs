7967db54c0b95c262eb58a4215be957f53a32680
Dheerajknoldus <dheeraj.mishra@knoldus.com>
2021-03-09 16:55:34 +0530

    formating,testing,clippy done alright

--- assignment4/src/rotation.rs
+++ assignment4/src/rotation.rs
@@ -9,12 +9,20 @@ use log::{debug, error, info, log_enabled, Level};
 // Return
 // This will return boolean{true, false} after checking condition that whether a "string2" is the rotation of "string1" or not.
 
-pub fn is_rotation(string1: &str, string2: &str) -> bool {
-    if string1.len() != string2.len() {
-        return false;
+pub fn is_rotation(string1: &str, string2: &str) -> String {
+    let mut collect_reverse_char = String::new();
+    let mut output = String::new();
+    for count in string1.chars().rev() {
+        collect_reverse_char.push(count);
     }
-    string1.repeat(2).contains(&string2)
+    if collect_reverse_char == string2 {
+        output.push_str("rotation is true");
+    } else {
+        output.push_str("rotation is false");
+    }
+    output
 }
+
 
 // Function maintaining the log for "is_rotation"
 fn repeat_finder_log() {
--- assignment4/src/test.rs
+++ assignment4/src/test.rs
@@ -3,13 +3,13 @@ use crate::{hello_count, palindrom};
 
 #[test]
 // Testing for duplicate character.
-fn duplicate_testing() {
+fn duplicate_testing_success() {
     let word_hello = String::from("hello world");
     output = hello_count::repeat_finder(&word_hello);
     assert_eq!(output, "lo")
 }
 #[test]
-fn duplicate_testing2() {
+fn duplicate_testing__success() {
     let word_hello = String::from("dheeraj");
     output = hello_count::repeat_finder(&word_hello);
     assert_eq!(output, "e")
@@ -18,14 +18,29 @@ fn duplicate_testing2() {
 #[test]
 // Testing for checking palindrome.
 
-fn pelindrom_testing() {
+fn pelindrom_testing_success() {
     let word = String::from("aabbbbaa");
     output = palindrom::check_palindrome(&word);
     assert_eq!(output, true)
 }
 #[test]
-fn pelindrom_testing2() {
+fn pelindrom_testing__success() {
     let word = String::from("aabbbbaaaabbbbaa");
     output = palindrom::check_palindrome(&word);
     assert_eq!(output, true)
+}
+#[test]
+//Testing to checking rotation
+fn rotation_success(){
+    let word = String::from("abcd");
+    let rotate = String::from("dcba");
+    output = is_rotation(&word, &rotate);
+    assert_eq!(output,"rotation is true")
+}
+#[test]
+fn rotation_failure(){
+    let word = String::from("abcd");
+    let rotate = String::from("dbaa");
+    output = is_rotation(&word, &rotate);
+    assert_eq!(output,"rotation is true")
 }
