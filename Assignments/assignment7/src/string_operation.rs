4249508afed268dde08be7d5af168ab95a22ba30
Dheerajknoldus <dheeraj.mishra@knoldus.com>
2021-03-13 14:35:12 +0530

    Testing performd successfully

--- Assignments/assignment7/src/string_operation.rs
+++ Assignments/assignment7/src/string_operation.rs
@@ -1,19 +1,35 @@
-
-pub fn desired_output_fn(first_slice: &str, second_slice: &str, third_slice: &str) -> String {
+// _get_conditional_string return the output on given condition.
+//
+// #Argument.
+// first_slice-Reference variable of type str.
+// second_slice-Reference variable of type str.
+// third_slice-Reference variable of type str.
+//
+// #Return
+// output-Type string.
+pub fn _get_conditional_string(first_slice: &str, second_slice: &str, third_slice: &str) -> String {
     let mut len_iter = 0;
     let mut index_value: usize = 0;
     let mut output: String = String::new();
     let mut _first_compare = 0;
-    if len_iter %2 ==0 {_first_compare = 0} else { _first_compare =1};
-    while index_value < first_slice.len() && index_value < second_slice.len() && index_value < second_slice.len() {
+    if len_iter % 2 == 0 {
+        _first_compare = 0
+    } else {
+        _first_compare = 1
+    };
+    while index_value < first_slice.len()
+        && index_value < second_slice.len()
+        && index_value < second_slice.len()
+    {
         match _first_compare {
             0 => {
-                let comp_res_str1_str2 =
-                    if first_slice.chars().nth(index_value) < second_slice.chars().nth(index_value) {
-                        first_slice.chars().nth(index_value)
-                    } else {
-                        second_slice.chars().nth(index_value)
-                    };
+                let comp_res_str1_str2 = if first_slice.chars().nth(index_value)
+                    < second_slice.chars().nth(index_value)
+                {
+                    first_slice.chars().nth(index_value)
+                } else {
+                    second_slice.chars().nth(index_value)
+                };
                 let res_char = if comp_res_str1_str2 < third_slice.chars().nth(index_value) {
                     comp_res_str1_str2
                 } else {
@@ -25,12 +41,13 @@ pub fn desired_output_fn(first_slice: &str, second_slice: &str, third_slice: &st
             }
 
             1 => {
-                let comp_res_str1_str2 =
-                    if first_slice.chars().nth(index_value) < second_slice.chars().nth(index_value) {
-                        second_slice.chars().nth(index_value)
-                    } else {
-                        first_slice.chars().nth(index_value)
-                    };
+                let comp_res_str1_str2 = if first_slice.chars().nth(index_value)
+                    < second_slice.chars().nth(index_value)
+                {
+                    second_slice.chars().nth(index_value)
+                } else {
+                    first_slice.chars().nth(index_value)
+                };
                 let result = if comp_res_str1_str2 > third_slice.chars().nth(index_value) {
                     comp_res_str1_str2
                 } else {
--- Assignments/assignment7/src/test.rs
+++ Assignments/assignment7/src/test.rs
@@ -1,6 +1,5 @@
-
 #[test]
-// Function Checks the success of substring generation
+// substring_generation_success Checks the success of substring generation
 fn substring_generation_success() {
     use crate::substring_generation;
     let word = "pa".to_string();
@@ -16,9 +15,23 @@ fn substring_generation_success_secondchars() {
 }
 
 #[test]
-// Function checks the patter_value success.
+// pattern_value_success checks the patter_value success.
 fn pattern_value_success() {
     use crate::pattern_searching;
     let output = pattern_searching::pattern_value();
     assert_eq!(output, 7);
+}
+
+#[test]
+//string_operation_success checks get_conditional_string success
+fn string_operation_success() {
+    use crate::string_operation;
+    let output = string_operation::_get_conditional_string("jjdhid", "ikjhjk", "rtysgi");
+    assert_eq!(output, "ijdhgd")
+}
+#[test]
+fn string_operation_secondry_success() {
+    use crate::string_operation;
+    let output = string_operation::_get_conditional_string("abcdef", "aioeu", "ramram");
+    assert_eq!(output, "aacda")
 }
