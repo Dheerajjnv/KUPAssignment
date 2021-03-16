46b73fdf6f50fad8e213cfcfc58b1fed363210ed
Dheerajknoldus <dheeraj.mishra@knoldus.com>
2021-03-16 19:32:19 +0530

    Feature added.

---
+++ Assignments/assignment10/src/creating_duplicate.rs
@@ -0,0 +1,17 @@
+/*pub fn element_duplication(mut list: Vec<i32>) -> Vec<i32> {
+    let mut element_storage: Vec<i32> = Vec::new();
+    let mut length = list.len();
+    while length != 0 {
+        let temp= list.pop()
+         match temp{
+            Some(temp)=>element_storage.push(temp),
+            None=> temp
+        };
+        element_storage.push(temp);
+        element_storage.push(temp);
+        length -= 1;
+    }
+    element_storage
+}
+
+*/
