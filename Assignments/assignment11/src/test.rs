194435032a4495385195bb4896e2b469d788f5f5
Dheerajknoldus <dheeraj.mishra@knoldus.com>
2021-03-18 01:23:23 +0530

    Logger added successfully

--- Assignments/assignment11/src/test.rs
+++ Assignments/assignment11/src/test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 #[test]
 fn first_repeated_searching_success() {
+    env_logger::builder().is_test(true).try_init();
     use crate::recursive_operation::enum_list::List::{Cons, Nil};
     use crate::recursive_operation::first_repeated::first_repeated_searching;
     let input = Cons(
@@ -14,9 +15,12 @@ fn first_repeated_searching_success() {
         )),
     );
     assert_eq!(first_repeated_searching(0, input), 21);
+    log::info!("This is first repeating element result")
+
 }
 #[test]
 fn second_repeated_searching_success() {
+    env_logger::builder().is_test(true).try_init();
     use crate::recursive_operation::enum_list::List::{Cons, Nil};
     use crate::recursive_operation::second_repeated::second_repeated_searching;
     let input = Cons(
@@ -36,9 +40,11 @@ fn second_repeated_searching_success() {
         )),
     );
     assert_eq!(second_repeated_searching(0, 0, input), 7);
+    log::info!("This is second repeating element result")
 }
 #[test]
 fn third_odd_searching_success() {
+    env_logger::builder().is_test(true).try_init();
     use crate::recursive_operation::enum_list::List::{Cons, Nil};
     use crate::recursive_operation::third_odd::third_odd_searching;
     let input = Cons(
@@ -49,9 +55,12 @@ fn third_odd_searching_success() {
         )),
     );
     assert_eq!(third_odd_searching(0, input), 3);
+    log::info!("This is third odd element result")
+
 }
 #[test]
 fn nth_number_finder_success() {
+    env_logger::builder().is_test(true).try_init();
     use crate::recursive_operation::enum_list::List::{Cons, Nil};
     use crate::recursive_operation::nth_element::nth_element_finder;
     let input = Cons(
@@ -62,4 +71,6 @@ fn nth_number_finder_success() {
         )),
     );
     assert_eq!(nth_element_finder(3, 0, input), 4);
+    log::info!("This is nth element element result")
+
 }
